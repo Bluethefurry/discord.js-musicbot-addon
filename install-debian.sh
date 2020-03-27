dd5aa4104e223a62751ade274d1d1e05d6979c99
Blue <13604413+Bluethefurry@users.noreply.github.com>
2020-03-27 13:11:43 +0100

    Update install-debian.sh

--- install-debian.sh
+++ install-debian.sh
@@ -5,5 +5,5 @@ sudo apt-get install nodejs ffmpeg -y
 npm uninstall discord.js node-opus opusscript discord.js-musicbot-addon
 npm install discord.js
 npm install opusscript
-npm install discord.js-musicbot-addon
+npm install https://github.com/Bluethefurry/discord.js-musicbot-addon
 echo "Done"
